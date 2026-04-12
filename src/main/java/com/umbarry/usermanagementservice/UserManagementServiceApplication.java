d2baf5badd5f40bf831fc6a2a145f7f9683d29d9
Umberto <uatania@outlook.com>
2026-04-12 19:15:45 +0200

    unused imports

--- src/main/java/com/umbarry/usermanagementservice/UserManagementServiceApplication.java
+++ src/main/java/com/umbarry/usermanagementservice/UserManagementServiceApplication.java
@@ -1,9 +1,7 @@
 package com.umbarry.usermanagementservice;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
